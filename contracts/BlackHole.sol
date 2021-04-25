af48525f2bb316db262d8efb7c01d137158812ac
xdexdev <xdexdexxdexdex@gmail.com>
2021-04-25 14:57:33 +0800

    reduce codes

--- contracts/BlackHole.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-
-// This is a black hole contract address
-// no one can transfer XDEX from the contract
-contract BlackHole {}
--- contracts/interface/IOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-
-
-interface IOracle {
-    function update(address tokenA, address tokenB) external;
-    function consult(address tokenIn, uint amountIn, address tokenOut) external view returns (uint amountOut);
-}
-
